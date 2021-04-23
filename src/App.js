0869249552a0f6eae2d118904374696ef6e46596
dhanashri <surpuredhanashri@gmail.com>
2021-04-23 20:31:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			categories: [],
+			jokes: {},
+		};
+	}
+
+	componentDidMount() {
+		fetch(`https://api.chucknorris.io/jokes/categories`)
+			.then((Response) => Response.json())
+			.then((data) => this.setState({ categories: data }));
+	}
+
+	handleChange = (e) => {
+		const caty = e.target.value;
+		const url = `https://api.chucknorris.io/jokes/random?category=` + caty;
+		fetch(url)
+			.then((Response) => Response.json())
+			.then((data) => this.setState({ jokes: data }));
+	};
+
+	render() {
+		return (
+			<div className="main-container">
+				<h1 className="headline">RANDOM JOKES</h1>
+				<select onChange={this.handleChange}>
+					<option>Select by catagory</option>
+					{this.state.categories.map((catagory) => (
+						<option key={catagory}>{catagory}</option>
+					))}
+				</select>
+				<div>
+					<img src={this.state.jokes.icon_url} alt='randamPics'></img>
+					<h1 className='main-content'>{this.state.jokes.value}</h1>
+				</div>
+			</div>
+		);
+	}
 }
-
-export default App;
